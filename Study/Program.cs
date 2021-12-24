46da81adf7879711fbe081fc4b7ae2a86a1e6c46
beaver7 <beaver007@beaverblock.com>
2021-12-24 09:46:23 +0900

    초기 커밋.

---
+++ Study/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ctrl + F5 : 콘솔창 띄우기
+namespace Study //프로젝트 이름으로 네임스페이스 생성됨
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+        }
+    }
+}
